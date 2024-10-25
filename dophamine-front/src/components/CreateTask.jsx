650d1289be5ab968224ad4e83f39ecbcb8a07cfa
Anton Bohomaz <bogomaz97@gmail.com>
2024-10-25 23:09:41 +0200

    Add styles for modal window

--- dophamine-front/src/components/CreateTask.jsx
+++ dophamine-front/src/components/CreateTask.jsx
@@ -1,9 +1,11 @@
+import React from 'react'
 import { Box } from "@mui/material";
+import "./CreateTask.css"
 
 function CreateTask() {
-  return <Box>
+  return <Box className="modalContainer">
     Here should be create task modal window 
   </Box>
 }
 
-export default CreateTask
+export default React.forwardRef((props, ref) => <CreateTask {...props} forwardedRef={ref} />)
